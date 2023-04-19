26b5c09681f2101b5e1e2c177f773fe5071f2e23
Leon Forsythe <lvforsythe95@gmail.com>
2023-04-19 13:48:18 -0400

    added function for retrieving list from firebase database and helper functions for appending and clearing list element

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://shopping-list-7d768-default-rtdb.firebaseio.com/",
@@ -8,6 +8,7 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const itemsInListDB = ref(database, "items")
+const booksInDB = ref(database, "books")
 
 
 const itemInput = document.getElementById("item-input");
@@ -16,7 +17,6 @@ const addBtn = document.getElementById("add-btn");
 const appendListToPage = (newItem) => {
   const list = document.getElementById("list")
 
-  
   list.innerHTML += `<li>${newItem}</li>`
 }
 
@@ -24,14 +24,28 @@ const clearField = () => {
   itemInput.value = ""
 }
 
+const getItemsInDB = () => {
+  onValue(itemsInListDB, function(snapshot){
+    let items = Object.values(snapshot.val())
+    clearList()
+    items.forEach((item) => {appendListToPage(item)})
+  })
+}
+
+const clearList = () => {
+  list.innerHTML = ""
+}
+
 const addToCart = (e) => {
   e.preventDefault();
 
   const newItem = itemInput.value;
   push(itemsInListDB, newItem)
-  
+
   clearField()
-  appendListToPage(newItem)
 };
 
+getItemsInDB()
+
 addBtn.addEventListener("click", addToCart);
+
